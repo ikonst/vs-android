1c628ea85cfdbec717cde24811e221df8050e374
Ilya Konstantinov <ilya.konstantinov@gmail.com>
2014-11-12 02:42:29 +0200

    Major overhaul: - Unify gcc properties (Gcc.Common.props, gcc.Arm.Common.props). - Organise Intellisense support by separating into android_gcc_compile.xml (for Intellisense) and android_gcc_compile_tool.xml (for actual compiler). - Produce preprocessor definition headers for ARM toolchains and features (gcc_features.h) for Intellisense consumption.
    Minor changes:
    - Remove ANDROID_NDK (since it's not defined by the NDK).
    - Add FormatWarnings and FormatSecurityWarnings enumerators.
    - Add documentation to some switches.

---
+++ MSBuild/Android/PlatformToolsets/gcc_features.h
@@ -0,0 +1,78 @@
+// This file contains carefully maintained preprocessor definitions
+// to mimic gcc built-in defines within Intellisense.
+//
+// The switches in android_gcc_compile.xml for various compiler
+// features should trigger the conditionals in this file.
+
+#ifdef ___cplusplus
+	#define _GNU_SOURCE 1
+	#define __DEPRECATED 1
+	#ifndef __VA_NO_EXCEPTIONS
+		#define __EXCEPTIONS 1
+	#endif
+	#define __GNUG__ 4
+	#ifndef __VA_NO_RTTI
+		#define __GXX_RTTI 1
+	#endif
+	#define __GXX_WEAK__ 1
+	#define __WCHAR_UNSIGNED__ 1
+	#undef __cplusplus
+	#define __cplusplus 199711L
+#else
+	#ifdef __VA_EXCEPTIONS
+		#define __EXCEPTIONS 1	
+	#endif
+#endif
+
+#ifdef __VA_ARM5
+	#define __ARM_ARCH 5
+	#define __ARM_ARCH_5TE__ 1
+	#define __ARM_ARCH_ISA_THUMB 1
+#endif
+
+#ifdef __VA_ARM7
+	#define __ARM_ARCH_PROFILE 65
+	#define __ARM_ARCH_ISA_THUMB 2
+	#define __ARM_ARCH 7
+	#define __ARM_ARCH_7A__ 1
+#endif
+
+#ifdef __VA_ARM7_HARD_FLOAT
+	#define __ARM_PCS_VFP 1
+#else
+	#define __ARM_PCS 1
+	#define __SOFTFP__ 1
+#endif
+
+#ifdef __VA_SHORT_ENUMS
+	#define __ARM_SIZEOF_MINIMAL_ENUM 1
+#else
+	#define __ARM_SIZEOF_MINIMAL_ENUM 4
+#endif
+
+#ifdef __VA_SHORT_WCHAR
+	#ifdef __arm__
+		#define __ARM_SIZEOF_WCHAR_T 2
+	#endif
+	#define __SIZEOF_WCHAR_T__ 2
+	#define __WCHAR_MAX__ 65535U
+	#define __WCHAR_TYPE__ unsigned short
+#else
+	#ifdef __arm__
+		#define __ARM_SIZEOF_WCHAR_T 4
+	#endif
+	#define __SIZEOF_WCHAR_T__ 4
+	#define __WCHAR_MAX__ 4294967295U
+	#define __WCHAR_TYPE__ unsigned int
+#endif
+
+#if defined(__VA_THUMB) && defined(__arm__)
+	#define __THUMBEL__ 1
+#endif
+
+#ifndef __VA_NO_PIC
+	#define __PIC__ 1
+	#define __pic__ 1
+#endif
+
+#include "intellisense.h"
---
+++ MSBuild/Android/PlatformToolsets/intellisense.h
@@ -0,0 +1,15 @@
+#ifndef __INTELLISENSE__
+#error This header is intended for Intellisense consumption. Do not include it.
+#endif
+
+// Undefine Microsoft predefined macros
+#undef _ATL_VER
+#undef _M_IX86
+#undef _M_X64
+#undef _MFC_VER
+#undef _MSC_VER
+#undef _MSC_EXTENSIONS
+#undef _NATIVE_WCHAR_T_DEFINED
+#undef _WCHAR_T_DEFINED
+#undef _WIN32
+#undef _WIN64
